f8d0d5aad838f6b91ca16195a836a937de456d17
Danilo Gonçalves Pinheiro <124277720+DaniloPin@users.noreply.github.com>
2025-06-24 10:42:55 -0300

    Add files via upload

---
+++ Funcionario.cs
@@ -0,0 +1,25 @@
+﻿public class Funcionario {
+
+    public string Nome { get; set; }
+    private double salario;
+
+    public Funcionario(string nome, double salarioInicial) {
+        Nome = nome;
+        this.salario = salarioInicial;
+    }
+
+    public void ReajustarSalario(double novoValor) { 
+        
+        if (novoValor > salario) {
+
+            salario = novoValor;
+        }
+        else {
+            Console.WriteLine("ERRO: O salário deve ser maior que o atual.");
+        }
+    }
+    public double Salario {
+        get { return salario; }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Funcionario func = new Funcionario("Débora Pinheiro", 4000);
+
+func.ReajustarSalario(3500);
+func.ReajustarSalario(4200);
+
+Console.WriteLine($"Funcionário: {func.Nome}");
+Console.WriteLine($"Novo salario: R$ {func.Salario.ToString("F2")} reais");
+
